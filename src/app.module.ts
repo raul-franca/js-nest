f7d18480c473c303e581732668b7f0965b4c8478
Raul França <raulmfranca@gmail.com>
2023-05-22 12:23:28 -0300

    add ormConfigs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { UsuarioModule } from './usuario/usuario.module';
+import { ProdutoModule } from './produto/produto.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PostegresConfigService } from './config/postegres.config.service';
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [
+    UsuarioModule,
+    ProdutoModule,
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: PostegresConfigService,
+      inject: [PostegresConfigService],
+    }),
+  ],
 })
 export class AppModule {}
